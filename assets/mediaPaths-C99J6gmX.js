1e8adb2ceaad321ce2a1c3e82b927e601593da7e
EdgarPC8 <edgartorrespc8@gmail.com>
2026-09-02 11:44:20 -0500

    deploy: chore: npm db:fix:raw-to-final en las 3 apps

---
+++ assets/mediaPaths-C99J6gmX.js
@@ -0,0 +1,1 @@
+import{D as t,z as r}from"./index-B89-TArr.js";function i(){return r().mediaFolderPrefix||t}function a(...e){return[i(),...e].filter(Boolean).join("/")}export{a as m};
--- assets/notificationProgramRequest-C5vNPTS9.js
+++
@@ -1,1 +0,0 @@
-import{o as t,I as a}from"./index-9k97BN4W.js";const r=()=>t.get("/notification-programs",a()),s=o=>t.post("/notification-programs",o,a()),c=(o,i)=>t.put(`/notification-programs/${o}`,i,a()),e=o=>t.delete(`/notification-programs/${o}`,a()),g=o=>t.post(`/notification-programs/${o}/send`,null,a());export{s as c,e as d,r as g,g as s,c as u};
